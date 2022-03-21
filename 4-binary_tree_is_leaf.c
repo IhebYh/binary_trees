ab8d7af906a2c63b8b5a9fcfff01d7d8afc134a7
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-03-21 22:36:52 +0100

    betty + code enhancement

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,7 +8,9 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
+	if (node == NULL)
+		return (0);
+	if (node->left != NULL || node->right != NULL)
 		return (0);
 	return (1);
 }

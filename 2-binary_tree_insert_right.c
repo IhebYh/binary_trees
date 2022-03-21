48f5e24a83ae4bc0693b6bfa461f3e755e7982de
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-03-21 20:21:35 +0100

    binary tree insert right + betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - a function that inserts a node as
+ * binary_tree_insert_right - a function that inserts a node as
  * the left-child of another node
  * @parent: (binary_tree_t) a pointer to thenode to insert
  * @value: (int) the value to store in thenew node
  * Return: pointer to the new node || node
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_node *node;
 
@@ -20,12 +20,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!node || !*node)
 		return (NULL);
 
-	node->left = parent->left;
-	if (node->left != NULL)
+	node->right = parent->right;
+	if (node->right != NULL)
 	{
-		node->left->parent = node;
+		node->right->parent = node;
 	}
-	parent->left = node;
+	parent->right = node;
 
 	return (node);
 }

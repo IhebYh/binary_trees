3af811443675c334b94b9cd39fa0637d1ca514b8
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-03-22 20:38:26 +0100

    size

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,8 +10,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	size_t left_height = tree_height(root->left);
-	size_t right_height = tree_height(root->right);
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
 
 	if (left_height >= right_height)
 		return (left_height + 1);

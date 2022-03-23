4853ff26bb9bad92174808d34b5d2666af43870a
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-03-23 21:03:19 +0100

    fixed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,9 +10,9 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->left != NULL || node->right != NULL)
-		return (0);
-	return (1);
+	else if (node->left == NULL || node->right == NULL)
+		return (1);
+	return (0);
 }
 /**
  * binary_tree_height - a function that measures the height of a binary tree

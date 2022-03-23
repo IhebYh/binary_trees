bb5899ffd8265df4f22969cc947ca17ed4d010e4
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-03-23 20:42:00 +0100

    fixed

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,18 +11,15 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	if (parent == NULL)
-		return (NULL);
-
 	node = malloc(sizeof(binary_tree_t));
 
 	if (node == NULL)
 		return (NULL);
 
+	node->parent = parent;
 	node->n = value;
 	node->left = NULL;
 	node->right = NULL;
-	node->parent = parent;
 
 	return (node);
 }

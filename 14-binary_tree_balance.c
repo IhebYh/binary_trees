7a198ae3880663bafff5ba3e8037b690510a58b3
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-03-23 21:44:02 +0100

    fixed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,16 +1,24 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_leaf - check node leaf or not
- * @node: node to check
- * Return: 1 if node if leaf 0 if not
+ * binary_treeHeight - a function that measures the height of a binary tree
+ * @tree: is pointer to the root node of the tree to measure the height
+ * Return: size_t
  */
-int binary_tree_is_leaf(const binary_tree_t *node)
+
+int binary_treeHeight(const binary_tree_t *tree)
 {
-	if (node == NULL)
+	int right_height, left_height;
+
+	if (tree == NULL)
 		return (0);
-	else if (node->left == NULL && node->right == NULL)
-		return (1);
-	return (0);
+
+	left_height = binary_treeHeight(tree->left);
+	right_height = binary_treeHeight(tree->right);
+
+	if (left_height >= right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
@@ -19,13 +27,12 @@ int binary_tree_is_leaf(const binary_tree_t *node)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t t_left, t_right;
+	int left, right;
 
-	if (tree == NULL || binary_tree_is_leaf(tree) == 1)
+	if (tree == NULL)
 		return (0);
-	t_left = binary_tree_balance(tree->left);
-	t_right = binary_tree_balance(tree->right);
-	if (t_left >= t_right)
-		return (t_left - t_right);
-	return (t_right - t_left);
+
+	left = binary_treeHeight(tree->left);
+	right = binary_treeHeight(tree->right);
+	return (left - right);
 }
